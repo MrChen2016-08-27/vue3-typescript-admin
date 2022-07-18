21c4bf62034d7e57f6259953ee97667614c4f3d8
chen jian bin <1909916440@qq.com>
2022-07-18 18:19:56 +0800

    update delModal component

--- src/components/DelModal.vue
+++ src/components/DelModal.vue
@@ -1,36 +1,48 @@
 <template>
-
-    <el-dialog title="操作提示" :model-value="modelValue" @update:modelValue="(val: boolean) => changeModalVisible(val)"
-        width="40%">
-        <label>即将删除&nbsp;{{ visibleState.delActionName }}&nbsp;</label>
+    <el-dialog
+        title="操作提示"
+        :model-value="props.modelValue"
+        @update:modelValue="(val: boolean) => changeModalVisible(val)"
+        width="40%"
+    >
+        <label>即将删除&nbsp;{{ props.delName }}&nbsp;</label>
         <p>您是否要删除?</p>
         <template #footer>
             <span class="dialog-footer">
-                <el-button :loading="visibleState.delLoading" @click="changeModalVisible(false)">取 消</el-button>
-                <el-button type="danger" :loading="visibleState.delLoading" @click="deleteRow">确 定</el-button>
+                <el-button
+                    :loading="visibleState.delLoading"
+                    @click="changeModalVisible(false)"
+                    >取 消</el-button
+                >
+                <el-button
+                    type="danger"
+                    :loading="visibleState.delLoading"
+                    @click="deleteRow"
+                    >确 定</el-button
+                >
             </span>
         </template>
     </el-dialog>
 </template>
 
 <script setup lang="ts">
-import { reactive, defineProps, withDefaults, defineEmits } from 'vue';
+import { reactive, defineProps, withDefaults, defineEmits } from "vue";
 
 const visibleState = reactive({
-    delActionName: '',
     delLoading: false,
 });
 
 interface PropsInterface {
-    delId?: number | null,
-    deleteRequestMethod: (id: number | string) => Promise<any>,
-    modelValue: boolean
+    delId?: number | null;
+    delName?: string | null;
+    deleteRequestMethod: (id: number | string) => Promise<any>;
+    modelValue: boolean;
 }
 
 const props = withDefaults(defineProps<PropsInterface>(), {});
 
 const emit = defineEmits<{
-    (e: 'update:modelValue', value: boolean): void
+    (e: "update:modelValue", value: boolean): void;
 }>();
 
 const deleteRow = async () => {
@@ -38,12 +50,10 @@ const deleteRow = async () => {
         visibleState.delLoading = true;
         await props.deleteRequestMethod(props.delId!);
         visibleState.delLoading = false;
-
     }
-}
+};
 
 const changeModalVisible = async (value: boolean) => {
-    emit('update:modelValue', value);
-}
-
+    emit("update:modelValue", value);
+};
 </script>
