62f06f8f1cd832f4e41fd1388f64286be53bfb23
chen jian bin <1909916440@qq.com>
2023-02-11 13:33:42 +0800

    update

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -1,6 +1,17 @@
-import axios from './axios.config'
-const base = 'upload'
+import axios from "./axios.config";
+const base = "upload";
 
 // 上传文件地址
-export const uploadImgAction = `${axios.defaults.baseURL}${base}/img`
-export const uploadFileAction = `${axios.defaults.baseURL}${base}/file`
+export const uploadImgAction = `${axios.defaults.baseURL}${base}/img`;
+export const uploadFileAction = `${axios.defaults.baseURL}${base}/file`;
+
+// 添加
+export const uploadImage = async (file: any) => {
+    let formData = new FormData();
+    formData.append("file", file);
+    return axios({
+        url: `${base}/img`,
+        method: "post",
+        data: formData,
+    });
+};
