38e747029d9a3beefcfa047580e8b2e94130330e
chen jian bin <1909916440@qq.com>
2023-02-22 10:44:25 +0800

    tab header update

--- src/components/TabNavigatorBar.vue
+++ src/components/TabNavigatorBar.vue
@@ -1,31 +1,41 @@
 <template>
     <div class="tab-navigator-component">
         <template v-for="(historyRecordItem, index) in getHistoryRoutes" :key="historyRecordItem.path">
-            <div :class="['tab-navigator-item', index == selectIndex && 'tab-navigator-item-active']"
-                @click.left="goTabRouter(historyRecordItem, index)" @click.right.prevent="handRightClick(index)">
+            <div ref="tabItemRefs" :class="['tab-navigator-item', index == selectIndex && 'tab-navigator-item-active']"
+                @click.left="goTabRouter(historyRecordItem, index)" @click.right.prevent="(e) => handRightClick(e, index)">
                 <div class="tab-navigator-item-content">
                     {{ historyRecordItem.title }}
                 </div>
-                <div class="close-container" v-show="((index == selectIndex) && getHistoryRoutes.length > 1)">
+                <div class="close-container" v-show="getHistoryRoutes.length > 1">
                     <el-icon @click.stop="removeRouterCard(historyRecordItem, index)">
                         <CloseBold />
                     </el-icon>
                 </div>
-                <div v-if="(index == rightClickIndex)" v-show="dropVisible" class="tab-drop-menu">
-                    <div @click="removeRouterCard(selectItem, rightClickIndex)">关闭本页</div>
-                    <div @click="removeTabRouters('right')">关闭右侧</div>
-                    <div @click="removeTabRouters('other')">关闭其他</div>
-                </div>
             </div>
         </template>
+        <Teleport to="body">
+            <div v-show="dropVisible" :style="{
+                top: `${dropModalState.y}px`,
+                left: `${dropModalState.x}px`
+            }" class="tab-drop-menu">
+                <div @click="removeRouterCard(selectItem, rightClickIndex)">关闭本页</div>
+                <div @click="removeTabRouters('right')">关闭右侧</div>
+                <div @click="removeTabRouters('other')">关闭其他</div>
+            </div>
+        </Teleport>
     </div>
-</template>
+<!-- <div v-if="dropVisible" class="tab-drop-menu" :style="{ 'left': absolData.left + 'px', 'top': absolData.top + 'px' }">
+        <div @click="removeRouterCard(selectItem, rightClickIndex)">关闭本页</div>
+        <div @click="removeTabRouters('right')">关闭右侧</div>
+        <div @click="removeTabRouters('other')">关闭其他</div>
+        
+    </div> --></template>
 
 
 <script setup lang="ts" >
 import { Getter, Actions, Mutations, HistoryRouterItem } from '@/store/app';
 import { fromJS } from 'immutable';
-import { onMounted, ref, watch } from 'vue';
+import { onMounted, reactive, ref, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 const router = useRouter();
 const route = useRoute();
@@ -34,6 +44,13 @@ const selectIndex = ref(0);
 const rightClickIndex = ref(0);
 const dropVisible = ref(false);
 let selectItem = ref<HistoryRouterItem | null>(null);
+const dropModalState = reactive({
+    x: 0,
+    y: 0
+});
+
+
+const tabItemRefs = ref([]);
 
 function goTabRouter(tabItem: HistoryRouterItem, tabItemIndex: number) {
     let historyItem = getHistoryRoutes.value[tabItemIndex];
@@ -43,7 +60,7 @@ function goTabRouter(tabItem: HistoryRouterItem, tabItemIndex: number) {
 };
 
 onMounted(() => {
-    document.addEventListener('click', function () {
+    document.addEventListener('click', function (e) {
         dropVisible.value = false;
     })
 })
@@ -94,12 +111,15 @@ watch(() => route.path, (newPath, oldPath) => {
     })
 })
 
-function handRightClick(index: number) {
+// 鼠标右击事件
+function handRightClick(e: MouseEvent, index: any) {
+    console.log('e', e);
+    dropModalState.x = e.pageX;
+    dropModalState.y = e.pageY;
     rightClickIndex.value = index;
     dropVisible.value = true;
+
 }
-
-
 
 
 
@@ -108,22 +128,46 @@ function handRightClick(index: number) {
 <style lang="less" scoped >
 @primary-active-color: #409eff;
 
+// .tab-navigator-component::-webkit-scrollbar {
+//     display: none; //去除占用宽度
+//     opacity: 0; //隐藏滚动条
+// }
+
 .tab-navigator-component {
     position: relative;
-    display: flex;
     width: 100%;
-
+    height: 100%;
+    align-items: center;
     border: 1px solid #cccccc;
+    border-radius: 0px 0px 20px 20px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
     cursor: default;
-    margin-bottom: 10px;
+    box-shadow: 0px 5px 14px 0px rgba(0, 0, 0, 0.05);
+    background-color: rgba(255, 255, 255, 0.8);
+    max-width: 100%;
+    // overflow-y: hidden;
 
     .tab-navigator-item {
         padding: 8px 20px;
         font-size: 14px;
-        max-width: 140px;
-        border-right: 1px solid #cccccc;
+        flex: 1;
+        max-width: 100px;
         position: relative;
         transition: all 0.3s ease;
+        opacity: 1;
+        color: #A0AEC0;
+        display: flex;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        position: relative;
+
+        &:hover {
+            cursor: pointer;
+            color: #409eff;
+        }
 
         .tab-navigator-item-content {
             overflow: hidden;
@@ -131,23 +175,29 @@ function handRightClick(index: number) {
             white-space: nowrap;
             position: relative;
 
+
         }
     }
 
     .tab-navigator-item-active {
-        background-color: #ffffff;
-        color: @primary-active-color;
+        color: #2D3748;
+        overflow: auto;
+        text-overflow: clip;
+        white-space: normal;
+        border-radius: 5px;
+        width: 100px;
+        flex: 0 0 100px;
     }
 }
 
 .tab-drop-menu {
     position: absolute;
-    z-index: 999;
+    z-index: 9999;
     background-color: #ffffff;
     box-shadow: 0 0 1px 1px rgba(220, 220, 220, 0.7);
     color: #333333;
     font-size: 12px;
-    cursor: default;
+    cursor: pointer;
     bottom: -100%px;
     left: 50%;
     width: 120px;
@@ -169,15 +219,39 @@ function handRightClick(index: number) {
     }
 }
 
-.close-container {
-    position: absolute;
-    right: 3px;
-    top: 2px;
-    z-index: 3;
-    font-size: 12px;
+.tab-navigator-item {
+    .close-container {
+        position: absolute;
+        right: 3px;
+        top: 11px;
+        z-index: 3;
+        font-size: 12px;
+        display: none;
+
+    }
 
     &:hover {
-        color: #333333;
+        .close-container {
+
+            display: block;
+
+        }
+    }
+
+}
+
+.tab-navigator-item-active {
+    .close-container {
+        position: absolute;
+        right: 3px;
+        top: 11px;
+        z-index: 3;
+        font-size: 12px;
+        display: block;
+
+        &:hover {
+            color: #333333;
+        }
     }
 }
 </style>
